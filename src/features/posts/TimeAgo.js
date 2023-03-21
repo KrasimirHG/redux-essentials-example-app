9f0580f4555b5416bce4755ecdc72a32fdd01c17
Krasimir <krasimir.genchev@softwaregroup.com>
2023-03-21 10:47:10 +0200

    semi

--- src/features/posts/TimeAgo.js
+++ src/features/posts/TimeAgo.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import { parseISO, formatDistanceToNow } from 'date-fns'
+import React from 'react';
+import { parseISO, formatDistanceToNow } from 'date-fns';
 
 export const TimeAgo = ({ timestamp }) => {
-  let timeAgo = ''
+  let timeAgo = '';
   if (timestamp) {
-    const date = parseISO(timestamp)
-    const timePeriod = formatDistanceToNow(date)
-    timeAgo = `${timePeriod} ago`
+    const date = parseISO(timestamp);
+    const timePeriod = formatDistanceToNow(date);
+    timeAgo = `${timePeriod} ago`;
   }
 
   return (
