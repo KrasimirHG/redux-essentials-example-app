9c336b4bb3d88f63bf33f6856eeb993002eb9da3
Krasio <krasimirhg@gmail.com>
2023-03-25 23:32:39 +0200

    fetch users by api

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,45 +1,52 @@
-import { createSlice, nanoid, createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice, nanoid, createAsyncThunk } from '@reduxjs/toolkit'
 import { client } from '../../api/client'
-import { sub } from 'date-fns';
+import { sub } from 'date-fns'
 
 const initialState = {
   posts: [],
   status: 'idle',
-  error: null
-};
+  error: null,
+}
 
 export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
-  const response = await client.get('/fakeApi/posts');
-  return response.data;
-});
+  const response = await client.get('/fakeApi/posts')
+  return response.data
+})
 
+export const addNewPost = createAsyncThunk(
+  'posts/addNewPost',
+  async (initialPost) => {
+    const response = await client.post('/fakeApi/posts', initialPost)
+    return response.data
+  }
+)
 const postsSlice = createSlice({
   name: 'posts',
   initialState,
   reducers: {
-    postAdded: {
-      reducer(state, action) {
-        state.posts.push(action.payload)
-      },
-      prepare(title, content, userId) {
-        return {
-          payload: {
-            id: nanoid(),
-            date: new Date().toISOString(),
-            title,
-            content,
-            user: userId,
-            reactions: {
-              thumbsUp: 0,
-              hooray: 0,
-              heart: 0,
-              rocket: 0,
-              eyes: 0,
-            },
-          },
-        }
-      },
-    },
+    // postAdded: {
+    //   reducer(state, action) {
+    //     state.posts.push(action.payload)
+    //   },
+    //   prepare(title, content, userId) {
+    //     return {
+    //       payload: {
+    //         id: nanoid(),
+    //         date: new Date().toISOString(),
+    //         title,
+    //         content,
+    //         user: userId,
+    //         reactions: {
+    //           thumbsUp: 0,
+    //           hooray: 0,
+    //           heart: 0,
+    //           rocket: 0,
+    //           eyes: 0,
+    //         },
+    //       },
+    //     }
+    //   },
+    // },
     reactionAdded(state, action) {
       const { postId, reaction } = action.payload
       const existingPost = state.posts.find((post) => post.id === postId)
@@ -54,28 +61,32 @@ const postsSlice = createSlice({
         existingPost.title = title
         existingPost.content = content
       }
-    }
+    },
   },
-    extraReducers(builder) {
-      builder
-          .addCase(fetchPosts.pending, (state, action) => {
-            state.status = 'loading'
-          })
-          .addCase(fetchPosts.fulfilled, (state, action) => {
-            state.status = 'succeeded'
-            state.posts = state.posts.concat(action.payload);
-          })
-          .addCase(fetchPosts.rejected, (state, action) => {
-            state.status = 'failed'
-            state.error = action.error.message
-          })
-    }
+  extraReducers(builder) {
+    builder
+      .addCase(fetchPosts.pending, (state, action) => {
+        state.status = 'loading'
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.status = 'succeeded'
+        state.posts = state.posts.concat(action.payload)
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.status = 'failed'
+        state.error = action.error.message
+      })
+      .addCase(addNewPost.fulfilled, (state, action) => {
+        state.posts.push(action.payload)
+      })
+  },
 })
 
-export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions;
+export const { postUpdated, reactionAdded } = postsSlice.actions
 
-export default postsSlice.reducer;
+export default postsSlice.reducer
 
-export const selectAllPosts = state => state.posts.posts;
+export const selectAllPosts = (state) => state.posts.posts
 
-export const selectPostById = (state, postId) => state.posts.posts.find(post => post.id === postId);
+export const selectPostById = (state, postId) =>
+  state.posts.posts.find((post) => post.id === postId)
